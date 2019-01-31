146ba17cebd2fb9127c575a7865201dece4e0c5b
SilentWolf <kezyolanipekun@gmail.com>
2019-01-31 15:00:10 +0100

    update made for services

--- src/app/services/api-handler.service.ts
+++ src/app/services/api-handler.service.ts
@@ -10,32 +10,32 @@ export class ApiHandlerService {
   constructor(private http: HttpClient) {
   }
 
-  post(url: string, request: any, options?: object = {}): Observable<any> {
+  post(url: string, request: any, options?: object): Observable<any> {
     return this.http.post(url, request, options);
   }
 
-  get_(url: string, options?: object = {}): Observable<any> {
+  get_(url: string, options?: object): Observable<any> {
     console.log({options});
     return this.http.get(url, options);
   }
 
-  get(url: string, request: object, options?: object = {}): Observable<any> {
+  get(url: string, request: object, options?: object): Observable<any> {
     return this.http.get(url, options);
   }
 
-  put(url: string, request: any, options?: object = {}): Observable<any> {
+  put(url: string, request: any, options?: object): Observable<any> {
     return this.http.put(url, request, options);
   }
 
-  patch(url: string, request: any, options?: object = {}): Observable<any> {
+  patch(url: string, request: any, options?: object): Observable<any> {
     return this.http.patch(url, request, options);
   }
 
-  delete(url: string, options?: object = {}): Observable<any> {
+  delete(url: string, options?: object): Observable<any> {
     return this.delete(url, null, options);
   }
 
-  delete(url: string, request: any, options?: object = {}): Observable<any> {
+  delete(url: string, request: any, options?: object): Observable<any> {
     return this.http.delete(url, options);
   }
 }
